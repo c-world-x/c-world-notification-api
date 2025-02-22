1bdf2b70d3d5e849d34d8987edb9d71ad8bb6440
Chi Nguyen <chi.nguyenh@one-line.com>
2025-02-22 17:01:26 +0700

    build: set up env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionFilter } from 'src/common/filters/all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const PORT = configService.get<number>("PORT");
+
+  const microservice = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: PORT,
+    },
+  });
+
+  microservice.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    })
+  );
+
+  microservice.useGlobalFilters(new AllExceptionFilter(configService));
+
+  await microservice.listen();
 }
 bootstrap();
